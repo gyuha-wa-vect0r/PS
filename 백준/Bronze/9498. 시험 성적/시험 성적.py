a99546d0ff5057daee1e417dd4c2f98fce7f07ac
Gyuha Lee <43605621+gyuha-wa-vect0r@users.noreply.github.com>
2022-12-14 19:36:14 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,16 @@
+a = input()
+
+if int(a) <= 100 and int(a) >= 90:
+    print("A")
+    
+elif int(a) <= 89 and int(a) >= 80:
+    print("B")
+    
+elif int(a) <= 79 and int(a) >= 70:
+    print("C")
+    
+elif int(a) <= 69 and int(a) >= 60:
+    print("D")
+    
+else:
+    print("F")

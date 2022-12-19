3fdfbb11e61bc174340a0c190872d4cb988ddbb8
Gyuha Lee <43605621+gyuha-wa-vect0r@users.noreply.github.com>
2022-12-19 10:48:12 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,17 @@
+a, b = input().split()
+x, y = int(a), int(b)
+mat1, mat2 = [], []
+
+for i in range(x):
+  input1 = list(map(int, input().split()))
+  mat1.append(input1)
+
+for j in range(x):
+  input2 = list(map(int, input().split()))
+  mat2.append(input2)
+
+for l in range(x):
+  for h in range(y):
+    print(mat1[l][h] + mat2[l][h], end = " ")
+
+  print()

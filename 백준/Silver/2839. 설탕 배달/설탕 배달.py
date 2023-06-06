861124fc4b58904e0a07e72d835aea6914a4ca4d
Gyuha Lee <43605621+gyuha-wa-vect0r@users.noreply.github.com>
2023-06-06 17:12:28 +0900

    Update 설탕 배달.py

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,14 @@
 weight = int(input())
-plasticBag = 0
+plasticBag = 0 # 봉지 개수
 
 while weight >= 0:
-  if weight % 5 == 0:
-    plasticBag += weight // 5 
-    print(plasticBag)
+  if weight % 5 == 0: # kg이 5로 다 담아지는 경우
+    plasticBag += weight // 5 # kg을 5로 나누어서 봉지 개수 저장
+    print(plasticBag) # 온전히 5로만 분배한 봉지 수 출력
     break 
-    
+  # kg이 5로 다 담아지지 않는 경우
   weight = weight - 3
-  plasticBag = plasticBag + 1 
-    
+  plasticBag = plasticBag + 1 # kg가 5의 배수가 될 때까지 kg-3, 3kg 봉지 개수+1 
+
 else:
   print(-1)

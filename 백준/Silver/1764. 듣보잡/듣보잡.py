b44ef5a2887ca945aff205db911845a201ce6cfe
Gyuha Lee <43605621+gyuha-wa-vect0r@users.noreply.github.com>
2023-06-08 14:57:31 +0900

    [Silver IV] Title: 듣보잡, Time: 3972 ms, Memory: 41884 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+nonListen = []
+nonSee = []
+for i in range(n):
+  name_nonListen = input()
+  nonListen.append(name_nonListen)
+for j in range(m):
+  name_nonSee = input()
+  nonSee.append(name_nonSee)
+
+nonAll_Sort = nonListen + nonSee
+nonAll_Sort.sort()
+nonAll = [0] + nonAll_Sort
+
+duplication = []
+for k in range(1, len(nonAll)): # k : 1 ~ 7
+  if nonAll[k-1] == nonAll[k]:
+    duplication.append(nonAll[k])
+
+print(len(duplication))
+for f in duplication:
+  print(f)

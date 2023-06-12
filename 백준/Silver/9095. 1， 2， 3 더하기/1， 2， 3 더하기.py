d60e13004bc320136721b45bf7523f11e4b0ef19
Gyuha Lee <43605621+gyuha-wa-vect0r@users.noreply.github.com>
2023-06-12 16:26:38 +0900

    Update 1， 2， 3 더하기.py

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -5,9 +5,9 @@ d[0] = 0; d[1] = 1; d[2] = 2; d[3] = 4 # n = 0 ~ 3까지 경우의 수 저장
 
 for _ in range(t):
   n = int(input())
-  if n <= 3:
+  if n <= 3: # n이 3 이하면 따로 저장한거 불러와서 출력
     print(d[n])
-  else:
+  else: # n이 4 이상이면 점화식 투입 후 출력
     for i in range(4, n+1):
       d[i] = d[i-1] + d[i-2] + d[i-3]
     print(d[i])
